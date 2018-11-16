5ce48fc21dec50b34d51b3140c97afc099cc041f
James Gardner <jamesgardner@live.com.au>
2018-11-16 11:57:17 +1100

    Support for subset matching long strings
    This meant creating an iterative rather than recursive version of find_longest_match.
    Full match algorithms are still recursive, since they aren't generally useful on long strings anyway.

--- dfa.py
+++ dfa.py
@@ -34,6 +34,24 @@ class DFAState:
         bigger_match = self.on_char[left[0]].find_longest_match(consumed+left[0], left[1:])
         return bigger_match or this_match
 
+    def find_longest_match_iter(self, consumed, left):
+        ''' 
+        Find longest possible match for string `left` given we've already traversed chars `consumed`
+        This version, unlike find_longest_match, works iteratively so it won't stack overflow on long strings
+        '''
+        state = self
+        biggest_match = None
+
+        while left:
+            if state.is_accepting:
+                biggest_match = consumed
+
+            consumed += left[0]
+            state = state.on_char[left[0]]
+            left = left[1:]
+
+        return biggest_match
+
 class DFA:
     def __init__(self, entry):
         self.entry = entry
@@ -46,7 +64,7 @@ class DFA:
         ''' For each position in s, finds the longest possible match, returning a list of matches '''
         matches = []
         while s:
-            match = self.entry.find_longest_match('', s)
+            match = self.entry.find_longest_match_iter('', s)
             if match not in (None, ''): # None is non-match, '' is match of length 0 (e.g. a* against b)
                 if not any(match in m for m in matches): # subset of something we've previously found
                     matches.append(match)
